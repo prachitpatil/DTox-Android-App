691a45763d5bbead78652a3ac9442f55b8715e3a
Prachit Patil <prachit.patil.1408@gmail.com>
2021-09-17 11:32:47 +0530

    Update signup_page.dart

--- dtox/lib/pages/signup_page.dart
+++ dtox/lib/pages/signup_page.dart
@@ -1,4 +1,5 @@
 import 'package:dtox/pages/home.dart';
+import 'package:dtox/pages/login_page.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
